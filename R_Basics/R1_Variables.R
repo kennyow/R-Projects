fc0be2f9dfa8cfefe19f8a5ff4e8cc06c78adb31
kennyow <114197131+kennyow@users.noreply.github.com>
2023-12-28 10:28:18 -0300

    Add R_Variables

---
+++ R_Basics/R1_Variables.R
@@ -0,0 +1,26 @@
+#Integers
+
+x <- 2L
+typeof(x)
+
+#Without 'L' it will be DOUBLE
+x <- 2
+typeof(x)
+
+#Double
+Y <- 2.5
+typeof(Y)
+
+#Complex
+z <- 3 + 2i
+typeof(z)
+
+#Character
+a <- "M"
+typeof(a)
+
+#Logical
+q1 <- T #True
+typeof(q1)
+q2 <- FALSE
+typeof(q2)
