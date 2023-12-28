5fbfb6b9885580c96839cc6a29db3195d4038066
kennyow <114197131+kennyow@users.noreply.github.com>
2023-12-28 10:42:22 -0300

    Add Using Variables

---
+++ R_Basics/R1_Using_Variables.R
@@ -0,0 +1,23 @@
+A <- 10
+B <- 5
+
+C <- A + B #Spaces don't matter
+
+#Print Out the result:
+C
+
+#You can select more than 1 variable to execute at the same time
+var1 <- 2.5
+var2 <- 4
+
+result <- var1 / var2
+result
+
+answer <-sqrt(var2)
+answer
+
+#Print the message (it already includes the space between the two words)
+greeting <-"Hello"
+name <- "Bob"
+message <- paste(greeting, name)
+message
