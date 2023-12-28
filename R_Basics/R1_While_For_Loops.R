effc29159bc0fcf5f181095f64bedd2e28e6281e
kennyow <114197131+kennyow@users.noreply.github.com>
2023-12-28 11:19:25 -0300

    Add files via upload

---
+++ R_Basics/R1_While_For_Loops.R
@@ -0,0 +1,31 @@
+
+
+#While Loop
+#----------------------------
+while(FALSE){
+  print("Hello")
+}
+
+while(TRUE){ #INFINITE LOOP
+  print("Hello")
+}
+
+#Counter Loop
+counter <- 1
+
+while(counter < 12){
+  print(counter)
+  counter <- counter + 1
+}
+
+# For Loop
+#----------------------------
+for(i in 1:5){
+  print("Hello R")
+}
+
+#If i execute this parte in separate, it will print in sequence
+1:5
+
+#The print includes all the number from the interval
+5:10
